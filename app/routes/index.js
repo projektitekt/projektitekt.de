961322512393b40a916243e2c8dc45507c5930a2
Tobias Bieniek <tobias.bieniek@gmail.com>
2020-04-05 18:49:17 +0200

    Remove unused `layout` service

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,18 +1,7 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
 
 export default class extends Route {
-  @service layout;
-
   model() {
     return this.store.findRecord('content', '_index');
-  }
-
-  activate() {
-    this.layout.wide = true;
-  }
-
-  deactivate() {
-    this.layout.wide = false;
   }
 }
