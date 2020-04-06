7a6778b516c7ee3621e17d0270a724abe6546f76
Tobias Bieniek <tobias.bieniek@gmail.com>
2020-04-06 14:08:24 +0200

    news: Add `loading` and `error` states

--- app/routes/news.js
+++ app/routes/news.js
@@ -2,9 +2,18 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default class extends Route {
+  @service fastboot;
   @service instagram;
 
-  model() {
-    return this.instagram.getPosts();
+  async model() {
+    try {
+      if (this.fastboot.isFastBoot) {
+        return { loading: true };
+      } else {
+        return { posts: await this.instagram.getPosts() };
+      }
+    } catch (error) {
+      return { error };
+    }
   }
 }
