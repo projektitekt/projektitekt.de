b503dca2fd44b3cb7105825d655cc84541467d5c
Tobias Bieniek <tobias.bieniek@gmail.com>
2020-04-18 14:12:13 +0200

    Add Sentry error tracking

--- app/app.js
+++ app/app.js
@@ -1,7 +1,18 @@
 import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
+
+import * as Sentry from '@sentry/browser'
+import { Ember as EmberIntegration } from '@sentry/integrations/esm/ember';
+
 import config from './config/environment';
+
+if (config.environment === 'production') {
+  Sentry.init({
+    dsn: 'https://45bb93af3b9f4070986722f65d768302@o379737.ingest.sentry.io/5204937',
+    integrations: [new EmberIntegration()]
+  });
+}
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
